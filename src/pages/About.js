23be92aab30a9776875600a472282a93a81f32b4
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2021-08-27 15:55:08 +0300

    Create react-project, build basic pages

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Create your own to Do List!</h1>
+        <p class="lead">
+          Verion <strong>1.0.0.</strong>
+        </p>
+      </div>
+    </div>
+  );
+};
