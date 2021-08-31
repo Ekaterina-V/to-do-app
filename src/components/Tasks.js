0ace455fa237b0da30f405b21aca9192169eca7c
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2021-08-31 14:56:04 +0300

    Fix className attribute

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export const Tasks = ({ tasks }) => {
   return (
-    <ul class="list-group">
+    <ul className="list-group">
       {tasks.map(task => (
         <li className="list-group-item task" key={task.id}>
           <div>
             <strong>{task.title}</strong>
             <span>{new Date().toLocaleDateString()}</span>
           </div>
-          <button type="button" class="btn btn-outline-warning btn-sm">
+          <button type="button" className="btn btn-outline-warning btn-sm">
             Delete
           </button>
         </li>
