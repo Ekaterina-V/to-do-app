90dc443fe85030ad9517e2ad0df863e1c3709424
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2021-09-02 21:55:17 +0300

    Add the try statement to test for errors

--- src/context/database/FirebaseState.js
+++ src/context/database/FirebaseState.js
@@ -1,8 +1,8 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import { FirebaseContext } from './firebaseContext';
 import { firebaseReducer } from './firebaseReducer';
-import axios from 'axios';
-import { SHOW_LOADER, REMOVE_TASK } from '../types';
+import { ADD_TASK, FETCH_TASK, REMOVE_TASK, SHOW_LOADER } from '../types';
 
 const url = process.env.TO_DO_APP_UR;
 
@@ -18,17 +18,39 @@ export const FirebaseState = ({ children }) => {
   const fetchTasks = async () => {
     showLoader();
     const res = await axios.get(`${url}/tasks.json`);
-    console.log('fetchTasks', res.data);
+
+    const payload = Object.keys(res.data).map(key => {
+      return {
+        ...res.data[key],
+        id: key,
+      };
+    });
+
+    dispatch({ type: FETCH_TASK, payload });
   };
 
   const addTask = async title => {
-    const task = { title, date: new Date().toJSON };
-    const res = await axios.post(`${url}/tasks.json`, task);
-    console.log('addTask', res.data);
+    const task = {
+      title,
+      date: new Date().toJSON(),
+    };
+
+    try {
+      const res = await axios.post(`${url}/tasks.json`, task);
+      const payload = {
+        ...task,
+        id: res.data.name,
+      };
+
+      dispatch({ type: ADD_TASK, payload });
+    } catch (e) {
+      throw new Error(e.message);
+    }
   };
 
   const removeTask = async id => {
     await axios.delete(`${url}/tasks/${id}.json`);
+
     dispatch({
       type: REMOVE_TASK,
       payload: id,
